f12baf3bc5ddf56187b7033a85b596203dd6119d
Cyrus Malekani <cyrus.malekani@gmail.com>
2024-08-18 21:07:03 +0200

    hand_anchor.gd added.

---
+++ characters/player/hand_anchor.gd
@@ -0,0 +1,10 @@
+class_name HandAnchor
+extends Node2D
+## Rotates the node around to face the direction of the mouse cursor
+
+func _process(delta: float) -> void:
+	var cursor_position = get_global_mouse_position()
+	#var direction_to = global_position.direction_to(cursor_position)
+	var rotation_angle = global_position.angle_to_point(cursor_position)
+	rotation = rotation_angle
+	
